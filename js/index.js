2e75db9c33e3c0f023143a56b81dae1346cce643
melvieira <melanie02125@gmail.com>
2022-04-06 21:45:08 -0400

    adding index.js file

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+const tasks = new TaskManager();
+
+console.log(tasks);
+
+
+
+// name, description, assignedTo, dueDate, status)
+const taskOne = new TaskManager("taskOne");
+
+taskOne.addTask('taskOne', 'Clean the fridge', 'Melanie', '4/12/2022', 'PENDING');
+console.log(taskOne);
+
+const taskTwo = new TaskManager("taskTwo");
+taskTwo.addTask('taskTwo', 'Do laundry', 'Geoffroy', '4/13/2022', 'PENDING');
+console.log(taskTwo);
